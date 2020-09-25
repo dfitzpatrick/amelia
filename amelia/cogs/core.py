eeef678211c35aa6554073f19516bc2a5725f122
Derek Fitzpatrick <dfitz.murrieta@gmail.com>
2020-09-25 14:00:42 -0700

    add pax role hack (temporary)

---
+++ amelia/cogs/core.py
@@ -0,0 +1,42 @@
+import asyncio
+import inspect
+import logging
+import textwrap
+import typing
+from datetime import datetime, timedelta
+
+import discord
+from discord.ext import commands
+
+from amelia.mixins.config import ConfigMixin
+log = logging.getLogger(__name__)
+
+class Core(commands.Cog):
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    async def assign_pax_role(self, member: discord.Member):
+        guild: discord.Guild = member.guild
+        # only r/flying
+        if guild.id != 379051048129789953:
+            return
+
+        try:
+            pax = discord.utils.get(guild.roles, id=759154599109328907)
+            log.debug(pax)
+            await member.add_roles(pax)
+        except (discord.errors.Forbidden, discord.errors.HTTPException):
+
+            return
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member):
+       await self.assign_pax_role(member)
+
+
+
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Core(bot))
